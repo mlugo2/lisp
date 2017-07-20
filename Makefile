7c52c81143ecde51fec688e387e8654b5731c092
mlugo2 <marcolugo02@gmail.com>
2017-07-19 23:40:04 -0500

    Added parsers and defined language

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	gcc -o main main.c -ledit
+	gcc -o main main.c mpc.c -ledit -lm
--- main.c
+++ main.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc.h"
 
 #include <editline/readline.h>
 #include <editline/history.h>
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
+
+	/* Create some Parsers */
+	mpc_parser_t* Number	= mpc_new("number");
+	mpc_parser_t* Operator	= mpc_new("operator");
+	mpc_parser_t* Expr		= mpc_new("expr");
+	mpc_parser_t* Lispy 	= mpc_new("lispy");
+
+	/* Define them with the following Language */
+	mpca_lang(MPCA_LANG_DEFAULT,
+		"														\
+			number    : /-?[0-9]+/ ;							\
+			operator  : '+' | '-' | '*' | '/' ;					\
+			expr      : <number> | '(' <operator> <expr>+ ')' ; \
+			lispy     : /^/ <operator> <expr>+ /$/ ;			\
+		",
+		Number, Operator, Expr, Lispy);
+
+
 	/* Print Version and Exit Information */
-	puts("Lispy Version 0.0.0.0.1");
+	puts("Lispy Version 0.0.0.0.2");
 	puts("Press Ctrl+c to Exit\n");
 
 	/* In a never ending loop */
@@ -19,11 +37,25 @@ int main(int argc, char const *argv[])
 		/* Add input to history */
 		add_history(input);
 
-		/* Echo input back to user */
-		printf("No you're a %s\n", input);
+		/* Attempt to parse the user input */
+		mpc_result_t r;
+		if (mpc_parse("<stdin>", input, Lispy, &r)) {
+			/* On success print and delete the AST */
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+		}
+		else {
+			/* Otherwise print and delete the Error */
+			mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
 
 		/* Free retrieved input */
 		free(input);
 	}
+
+	/* Undefine and delete our parsers */
+	mpc_cleanup(4, Number, Operator, Expr, Lispy);
+
 	return 0;
 }
